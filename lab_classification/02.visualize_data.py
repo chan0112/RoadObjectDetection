cbd0e964512980c525c2e296540f1e2e286bb1d8
chan0112 <98633556+chan0112@users.noreply.github.com>
2022-02-05 17:56:28 +0900

    week3

---
+++ lab_classification/02.visualize_data.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+
+train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    '../classification_data/',image_size=(224,224),label_mode='categorical')
+
+data = train_dataset.take(1)
+
+plt.figure(0)
+plt.title('data')
+
+for images, labels in data:
+    for i in range(16):
+        plt.subplot(4,4,i+1)
+        plt.imshow(images[i].numpy().astype('uint8'))
+        plt.title(train_dataset.class_names[np.argmax(labels[i])])
+        plt.axis('off')
+
+plt.show()
