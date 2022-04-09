f02ebddd48be14b7c7e8e243970ea7e93f69a773
chan0112 <98633556+chan0112@users.noreply.github.com>
2022-04-09 17:36:08 +0900

    final

--- lap_data/07.resize_image.py
+++ lap_data/07.resize_image.py
@@ -3,7 +3,7 @@ import cv2
 file_name = 'ae7f4103-b1fbdc93.jpg'
 image = cv2.imread('../data/images/' + file_name)
 
-resize_image = cv2.resize(image, (100, 200))
+resize_image = cv2.resize(image, (500, 500))
 
 cv2.imshow('image',image)
 cv2.imshow('resize_image', resize_image)
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import json
-
-f = open('../data/labels/labels.json')
-label = json.load(f)
-
-f_name = input()
-for i in range(len(label)):
-    if label[i]['name'] == f_name:
-        print(label[i]['labels'][0]['box2d'])
+# import json
+# #
+# # f = open('../data/labels/labels.json')
+# # label = json.load(f)
+# #
+# # f_name = input()
+# # for i in range(len(label)):
+# #     if label[i]['name'] == f_name:
+# #         print(label[i]['labels'][0]['box2d'])
+print('hello python')
